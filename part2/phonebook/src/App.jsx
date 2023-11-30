334a1d0df60b8076a1713614b68305e4b5e272ef
Niclas-L <lemstromniclas@gmail.com>
2023-11-30 13:41:57 +0200

    Finished 2_6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    console.log("persons:", persons);
+    console.log("newName:", newName);
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const Person = ({ person }) => {
+    return <p>{person.name}</p>;
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <Person key={person.name} person={person} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
