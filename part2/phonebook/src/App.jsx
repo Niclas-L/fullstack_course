0e9708ef313ff5677a697cf6db54447570ca3ae5
Niclas-L <lemstromniclas@gmail.com>
2023-11-30 20:21:24 +0200

    Finished 2_8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,29 +1,41 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
     console.log("button clicked", event.target);
     console.log("persons:", persons);
     console.log("newName:", newName);
+    console.log("newNumber:", newNumber);
     if (existingPerson(newName)) {
       alert(`${newName} is already added to phonebook`);
       setNewName("");
+      setNewNumber("");
       return;
     }
     const personObject = {
       name: newName,
+      number: newNumber,
     };
     setPersons(persons.concat(personObject));
     setNewName("");
+    setNewNumber("");
   };
 
   const handleNameChange = (event) => {
-    console.log(event.target.value);
+    console.log("Name event.target.value:", event.target.value);
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    console.log("Number event.target.value:", event.target.value);
+    setNewNumber(event.target.value);
   };
 
   const existingPerson = (name) => {
@@ -31,7 +43,11 @@ const App = () => {
   };
 
   const Person = ({ person }) => {
-    return <p>{person.name}</p>;
+    return (
+      <p>
+        {person.name} {person.number}
+      </p>
+    );
   };
 
   return (
@@ -41,6 +57,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
