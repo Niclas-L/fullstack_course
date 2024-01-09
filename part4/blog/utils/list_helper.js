8694b4c4a83dbf7f6d79de9276a0fd6c7e444a3b
Niclas-L <lemstromniclas@gmail.com>
2024-01-09 19:29:06 +0200

    Finished 4_5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,17 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const result = blogs.reduce(
+    (currentFavorite, blog) =>
+      currentFavorite.likes > blog.likes ? currentFavorite : blog,
+    blogs[0]
+  );
+  return result;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
